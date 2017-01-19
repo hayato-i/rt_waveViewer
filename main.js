757556912d9f5f14ffb9e7417fd4b4295f6fbbde
Hayato Izumi <hayto-i@digirea.com>
2017-01-19 18:49:26 +0900

    @アナライザー苦戦中

--- main.js
+++ main.js
@@ -12,13 +12,34 @@ window.AudioContext = window.AudioContext       ||
 const WIDTH = 640;
 const HEIGHT = 360;
 
-(function(){
+window.onload = function(){
 
     // AudioContext生成
     let context = new AudioContext();
     
     // アナライザー生成
-    let analyzer = context.createAnalyser();
+    let analyser = context.createAnalyser();
+
+    // 離散フーリエ変換精度
+    analyser.fftSize = 1024;
+    
+    // Canvas初期化
+    let cid = document.getElementById('analyser');
+    let canvas = cid.getContext("2d");
+
+    // グラデーション
+    let gradbase = canvas.createLinearGradient(0, 0, 0, 256);
+    gradbase.addColorStop(0, "rgb(20, 22, 20)");
+    gradbase.addColorStop(1, "rgb(20,20,200)");
+
+    let gradline = [];
+
+    for(let i = 0; i < 256; ++i) {
+        gradline[i] = canvas.createLinearGradient(0, 256 - i, 0, 256);
+        let n = (i & 64) * 2;
+        gradline[i].addColorStop(0, "rgb(255,0,0)");
+        gradline[i].addColorStop(1, "rgb(255," + i + ",0)");
+    }
 
     // マイクにアクセス
     let mic = {audio:true, camera:false};
@@ -33,22 +54,31 @@ const HEIGHT = 360;
         let source = context.createMediaStreamSource(stream);
 
         // マイク音声をアナライザーノードに接続
-        source.connect(analyzer);
+        source.connect(analyser);
 
         // 出力ノードににマイク音声ノードを接続
-        analyzer.connect(context.destination);
+        analyser.connect(context.destination);
 
+        setInterval(function(){
+            canvas.fillStyle = gradbase;
+            canvas.fillRect(0, 0, 255, 255);
+            let audioAnalyseData = new Uint8Array(analyser.frequencyBinCount);
+            analyser.getByteTimeDomainData(audioAnalyseData); //Waveform Data
+            for(var i = 0; i < 256; ++i) {
+                canvas.fillStyle = gradline[audioAnalyseData[i]];
+                canvas.fillRect(i, 256 - audioAnalyseData[i], 1, audioAnalyseData[i]);
+            }
+        },100);
 
- 
     }
 
     /**
     * @param {NavigatorUserMediaError|MediaStreamError} error
     */
     let  errorCallback = function(error) {
-        // do something ....
+        console.log("No mic!!");
     };
     
     navigator.getUserMedia(mic, successCallback, errorCallback);
 
-})();
+}();
