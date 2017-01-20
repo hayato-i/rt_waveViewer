46b6e0fd3fa6491f3912827274b2cfad099d6d84
Hayato Izumi <hayto-i@digirea.com>
2017-01-20 10:42:50 +0900

    @ヴィジュアライザできとるやんけー

--- main.js
+++ main.js
@@ -9,8 +9,20 @@ window.AudioContext = window.AudioContext       ||
                       window.webkitAudioContext;
 
 // canvasサイズの定義
-const WIDTH = 512;
-const HEIGHT = 512;
+const WIDTH = 640;
+const HEIGHT = 360;
+
+// いろいろスムーズに描画するための設定　ちゃんと把握してね
+const FFTSIZE = 2048;
+const SMOOTHING = 0.8;
+
+/* 波形表示か周波数表示かの切り替えボタンにしたい
+function onLoaded() {
+    let button = document.querySelector('button');
+    button.removeAttribute('disabled');
+    button.innerHTML = 'Wave/Freq';
+};
+*/
 
 (function(){
 
@@ -22,25 +34,21 @@ const HEIGHT = 512;
         let analyser = context.createAnalyser();
 
         // 離散フーリエ変換精度
-        analyser.fftSize = 1024;
+        analyser.smoothingTimeConstant = SMOOTHING;
+        analyser.fftSize = FFTSIZE;
     
+        // 最大音量、最小音量の設定
+        analyser.minDecibels = -140;
+        analyser.maxDecibels = 0;
+
+
         // Canvas初期化
-        let cid = document.getElementById('analyser');
-        let ctx = cid.getContext("2d");
+        let canvas = document.getElementById('analyser');
+        let drawContext = canvas.getContext("2d");
 
-        // グラデーション
-        let gradbase = ctx.createLinearGradient(0, 0, 0, 256);
-        gradbase.addColorStop(0, "rgb(20, 22, 20)");
-        gradbase.addColorStop(1, "rgb(20,20,200)");
-
-        let gradline = [];
-
-        for(let i = 0; i < 512; ++i) {
-            gradline[i] = ctx.createLinearGradient(0, 512 - i, 0, 512);
-            let n = (i & 128) * 2;
-            gradline[i].addColorStop(0, "rgb(255,0,0)");
-            gradline[i].addColorStop(1, "rgb(255," + i + ",0)");
-        }
+        // Canvas サイズ
+        canvas.width = WIDTH;
+        canvas.height = HEIGHT;
 
         // マイクにアクセス
         let mic = {audio:true, camera:false};
@@ -49,6 +57,7 @@ const HEIGHT = 512;
         * @param {MediaStream|LocalMediaStream} stream
         */
 
+        // マイク認識ができたら常に描画
         let successCallback = function(stream){
 
             // MediaStreamAudioSourceNodeのインスタンスを生成
@@ -61,33 +70,49 @@ const HEIGHT = 512;
             analyser.connect(context.destination);
 
             setInterval(function draw(){
-                ctx.fillStyle = gradbase;
-                ctx.fillRect(0, 0, 511, 511);
-                let audioAnalyseData = new Uint8Array(analyser.frequencyBinCount);
-                analyser.getByteTimeDomainData(audioAnalyseData); //Waveform Data
-                for(var i = 0; i < 512; ++i) {
-                    ctx.fillStyle = gradline[audioAnalyseData[i]];
-                    ctx.fillRect(i, 512 - audioAnalyseData[i], 1, audioAnalyseData[i]);
-                }
-            },100);
+                
+                // 音声波形と、周波数波形のデータ確保
+                let freqs = new Uint8Array(analyser.frequencyBinCount); // 周波数データ
+                let times = new Uint8Array(analyser.frequencyBinCount); // 音声波形データ
+                
+                analyser.getByteFrequencyData(freqs); // Frequency Data
+                analyser.getByteTimeDomainData(times); // Waveform Data
 
+                let width = Math.floor(1/freqs.length, 10);
+
+                // Draw the frequency domain chart.
+                for (let i = 0; i < analyser.frequencyBinCount; i++) {
+                    let value = freqs[i];
+                    let percent = value / 256;
+                    let height = HEIGHT * percent;
+                    let offset = HEIGHT - height - 1;
+                    let barWidth = WIDTH/analyser.frequencyBinCount;
+                    let hue = i/analyser.frequencyBinCount * 360;
+                    drawContext.fillStyle = 'hsl(' + hue + ', 100%, 50%)';
+                    drawContext.fillRect(i * barWidth, offset, barWidth, height);
+                }
+
+                // Draw the time domain chart.
+                for (let i = 0; i < analyser.frequencyBinCount; i++) {
+                    let value = times[i];
+                    let percent = value / 256;
+                    let height = HEIGHT * percent;
+                    let offset = HEIGHT - height - 1;
+                    let barWidth = WIDTH/analyser.frequencyBinCount;
+                    drawContext.fillStyle = 'white';
+                    drawContext.fillRect(i * barWidth, offset, 1, 2);
+                }
+
+
+            },50);
         }
 
         /**
         * @param {NavigatorUserMediaError|MediaStreamError} error
         */
+        // マイクが無いよ！
         let  errorCallback = function(error) {
             console.log("No mic!!");
-            setInterval(function draw(){
-                ctx.fillStyle = gradbase;
-                ctx.fillRect(0, 0, 511, 511);
-                let audioAnalyseData = new Uint8Array(analyser.frequencyBinCount);
-                analyser.getByteTimeDomainData(audioAnalyseData); //Waveform Data
-                for(var i = 0; i < 512; ++i) {
-                    ctx.fillStyle = gradline[audioAnalyseData[i]];
-                    ctx.fillRect(i, 512 - audioAnalyseData[i], 1, audioAnalyseData[i]);
-                }
-            },100);
         };
     
         navigator.getUserMedia(mic, successCallback, errorCallback);
